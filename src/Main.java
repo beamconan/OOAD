648e44fa845dde2acafd1d690925071cce323967
EncodP <pisit.sr40@gmail.com>
2018-11-05 18:18:25 +0700

    update Cinema

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 package pkg10192018java;
-
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author pisit
@@ -8,11 +9,17 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        Movie new1 = new Movie(1);
+       /* Movie new1 = new Movie(1);
         Movie new2 = new Movie(2, "english", "thai");
         System.out.println(new1);
-        Showtime st1 = new Showtime(new2);
-        
+        Showtime st1 = new Showtime(new2);*/
+       List<String> list = new ArrayList<String>();
+       
+        Cinema cinema = Cinema.getInstance();
+        cinema.addTest("Test1");
+        cinema.addTest("Test2");
+        list = cinema.getTest();
+        System.out.println(list);
     }
     
 }
